ff69a9efb9abf05f34e0190c757932b973f514c1
dechentf <dechentf@physik.hu-berlin.de>
2020-10-11 16:10:27 +0200

    host utils updated - hadamard_func_rhs, get_max

--- lib/utils/host_utils.h
+++ lib/utils/host_utils.h
@@ -1,11 +1,27 @@
 #ifndef _HOST_UTILS_H_
 #define _HOST_UTILS_H_
 
-
-template<class real_type>
-void apply_pointwise(const real_type* in,real_type* out,int size,pointwise<real_type> func){
-    for(int i=0;i<size;i++) out[i]=func(in[i]);
+template <class real_type>
+void apply_pointwise(const real_type *in, real_type *out, int size, pointwise<real_type> func)
+{
+    for (int i = 0; i < size; i++)
+        out[i] = func(in[i]);
 }
 
+template <class real_type>
+void hadamard_func_rhs(const real_type *lhs, real_type *rhs, real_type *res, int size, pointwise<real_type> dev_func)
+{
+    for (int i = 0; i < size; i++)
+        res[i] = lhs * func(rhs[i]);
+}
+
+template <class real_type>
+real_type get_max(const real_type *data, int length)
+{
+    real_type max = 0;
+    for (int i = 0; i < length; i++)
+        max = (data[i] > max ? data[i] : max);
+    return max;
+}
 
 #endif // _HOST_UTILS_H_
